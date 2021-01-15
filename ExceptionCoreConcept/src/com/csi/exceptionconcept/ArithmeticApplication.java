ca801d6b8967b9e628241cb87de0b113f0b0970a
admin <admin@DESKTOP-0VAU8ER>
2021-01-15 10:27:06 +0530

    Code changes as per business requirements

---
+++ ExceptionCoreConcept/src/com/csi/exceptionconcept/ArithmeticApplication.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.csi.exceptionconcept;
+
+/**
+ * @author admin
+ *
+ */
+public class ArithmeticApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n1 = 20, n2 = 10, result = 0;
+
+		result = n1 + n2;
+		System.out.println(result);
+	}
+
+}
--- ExceptionCoreConcept/src/com/csi/exceptionconcept/JDBCConcept.java
+++ ExceptionCoreConcept/src/com/csi/exceptionconcept/JDBCConcept.java
@@ -5,10 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JDBCConcept {
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	public static void main(String[] args){
 
 		
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection con=DriverManager.getConnection("jdbc://localhost:3306/empdb", "root", "root");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc://localhost:3306/empdb", "root", "root");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
